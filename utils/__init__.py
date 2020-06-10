bde08adc5edd40230bae4023c730decd241e0c59
Alex Casey <alex.casey.13@gmail.com>
2020-06-09 21:43:41 -0400

    fixed Fourier path starting point

--- utils/__init__.py
+++ utils/__init__.py
@@ -156,7 +156,211 @@ def strip_harmonics(descriptor, n_h = None):
 
     return descriptor_stripped
 
+def get_area_and_centroid(path):
+    """green's theorem
+    https://leancrew.com/all-this/2018/01/greens-theorem-and-section-properties/
+    """
+    s = 0
+    sx = 0
+    sy = 0
+    for i in range(path.shape[1]-1):
+        factor = path[0][i]*path[1][i+1] - path[0][i+1]*path[1][i]
+        s += factor;
+        sx += ((path[0][i] + path[0][i+1]) * factor);
+        sy += ((path[1][i] + path[1][i+1]) * factor);
+
+    area = s/2
+    return area, sx/(6*area), sy/(6*area)
+
 def fix_starting_loc(path):
+    """ok, most of this code comes from likes 551-690 in
+    Jesus' get_pore_data.py module. Honestly, I think it could be
+    re-done to use only half of the lines but I'm not going to break
+    something that is working.
+    However, I'm not using the "check for
+    unique points" portion. The added for loops and zipping and unzipping
+    is super redundant. The duplicate points case will only result in the
+    path already being closed. So it can be accounted for by not adding a
+    closure point.
+
+    The only other things to keep in mind is that the incoming path should
+    be CLOSED. The operations work on OPEN paths so the first thing this
+    function does is drop the last point."""
+
+    # get centroid information
+    # this code depends on the shape being centered
+    area, xc, yc = get_area_and_centroid(path)
+
+    # make path open instead of closed
+    path = path[:,:-1]
+
+    #Unpack the perimeter path information and normalize the coordiantes to the
+    # void centroid location.
+    x_perim = path[0]-xc
+    y_perim = path[1]-yc
+
+    #Locate the right-most point (most positive x-position) along the contour.
+    rm_path_ind = np.where(x_perim==x_perim.max())[0][0]
+    print(rm_path_ind)
+
+    #Determine if the x_axis crossing is in the clockwise (-1) or
+    # counter-clockwise (1) direction.  This is achieved by noting if the
+    # right-most point is above or below the axis.  As the centroid of the void
+    # is shifted to (0,0), this approach is robust. Also, detect if the
+    # right-most point is coincidently the x-axis crossing point
+    closed = False
+    if y_perim[rm_path_ind] == 0:
+
+        #Set the right-most point as the crossing point
+        x_par_start = x_perim[rm_path_ind]
+
+        #Set the index of the nearest y-positive path point as the index of x_0
+        x_0_index = rm_path_ind + 1
+
+        # oh Jesus... we need to fix above, what if it is the last point?!?!
+        # this should do the trick
+        x_0_index = x_0_index % x_perim.size
+
+        closed = True
+
+    elif y_perim[rm_path_ind] > 0:
+
+        #Starting at the right-most point, find the point where the perimeter
+        # path crosses the x-axis in the counter-clockwise direction, even if
+        # this point is between two points along the perimeter path. To
+        # accomplish this, create a shifted perimeter path starting at the
+        # location of the right-most point along the determined direction to
+        # the x-axis.
+
+        #Create a variable to hold the shifted path
+        x_shift_path = np.zeros(np.shape(x_perim))
+        y_shift_path = np.zeros(np.shape(y_perim))
+
+        #Populate the shifted path variables
+        x_shift_path[0:rm_path_ind+1] = np.flipud(x_perim[:rm_path_ind+1])
+        x_shift_path[rm_path_ind+1:] = np.flipud(x_perim[rm_path_ind+1:])
+        y_shift_path[0:rm_path_ind+1] = np.flipud(y_perim[:rm_path_ind+1])
+        y_shift_path[rm_path_ind+1:] = np.flipud(y_perim[rm_path_ind+1:])
+
+        #Locate the index of the path points on either side of the x-axis
+        start_ind_1 = np.where(y_shift_path<0)[0][0]
+        start_ind_0 = start_ind_1-1
+        print(start_ind_1)
+
+        #Determine the locations of the points on either side of the x-axis
+        x_0 = x_shift_path[start_ind_0]
+        y_0 = y_shift_path[start_ind_0]
+        x_1 = x_shift_path[start_ind_1]
+        y_1 = y_shift_path[start_ind_1]
+
+        #Determine the cross point along the x-axis by finding where the line
+        # from the path points on either side of the x-axis intersects the
+        # x-axis. If the line is vertical, then the x-location of the crossing
+        # is the same as the x-location of the neighboring path points
+        if x_0 == x_1:
+            x_par_start = x_0
+
+        #If not, find the x-location of the crossing by geometry
+        else:
+            x_par_start = -(y_0 - x_0 * ((y_1-y_0)/(x_1-x_0)))/((y_1-y_0)/(x_1-x_0))
+
+        #Find the index of the x_0/y_0 point from the perimeter path where the
+        # x_0/y_0 point corresponds to the nearest y-positive path point to the
+        # x-axis cross point.
+
+        # another alex fix to not have to check for unique points.
+        if y_shift_path[start_ind_0] == 0:
+            start_ind_0 -= 1
+            # note, cannot go negative because this case would have triggered
+            # the first if statement
+            closed = True
+
+        # I (alex) added the arange call. It's ugly but it fixes a
+        # failure point in Jesus' code where rm_path_ind - start_ind_0
+        # can represent a negative index.
+        x_0_index = np.arange(x_perim.size)[rm_path_ind - start_ind_0]
+
+    else:
+
+        #Starting at the right-most point, find the point where the perimeter
+        # path crosses the x-axis in the clockwise direction, even if this
+        # point is between two points along the perimeter path. To accomplish
+        # this, create a shifted perimeter path starting at the location of the
+        # right-most point along the determined direction to the x-axis.
+
+        #Create a variable to hold the shifted path
+        x_shift_path = np.zeros(np.shape(x_perim))
+        y_shift_path = np.zeros(np.shape(y_perim))
+
+        #Determine the remaining number of points along the perimeter path past
+        # the right-most point
+        N_pastrm = np.size(x_perim)-rm_path_ind
+
+        #Populate the shifted path variables
+        x_shift_path[0:N_pastrm] = x_perim[rm_path_ind:]
+        x_shift_path[N_pastrm:] = x_perim[0:rm_path_ind]
+        y_shift_path[0:N_pastrm] = y_perim[rm_path_ind:]
+        y_shift_path[N_pastrm:] = y_perim[0:rm_path_ind]
+
+        #Locate the index of the path points on either side of the x-axis
+        start_ind_1 = np.where(y_shift_path>0)[0][0]
+        start_ind_0 = start_ind_1-1
+
+        #Determine the locations of the points on either side of the x-axis
+        x_0 = x_shift_path[start_ind_0]
+        y_0 = y_shift_path[start_ind_0]
+        x_1 = x_shift_path[start_ind_1]
+        y_1 = y_shift_path[start_ind_1]
+
+        #Determine the cross point along the x-axis by finding where the line
+        # from the path points on either side of the x-axis intersects the
+        # x-axis
+
+        #If the line is vertical, then the x-location of the crossing is the
+        # same as the x-location of the neighboring path points
+        if x_0 == x_1:
+            x_par_start = x_0
+        else:
+            x_par_start = -(y_0 - x_0 * ((y_1-y_0)/(x_1-x_0)))/((y_1-y_0)/(x_1-x_0))
+
+        # another alex fix to not have to check for unique points.
+        if y_shift_path[start_ind_0] == 0:
+            closed = True
+
+        #Find the index of the x_0/y_0 point from the perimeter path where the
+        # x_0/y_0 point corresponds to the nearest y-positive path point to the
+        # x-axis cross point.
+        # another alex fix to account for if it is the last point
+        x_0_index = (rm_path_ind + start_ind_1) % x_perim.size
+
+    #Create a parameterized perimeter path starting at the right-most
+    # crossing point of the x-axis (x_start,y_start) along the perimeter
+    # path and ends at the starting point ot form a closed contour.
+
+    #The y-location of the crossing is, by definition, zero.
+    y_par_start = 0
+
+    #Determine how many points are from the x_0/y_0 point to the last point
+    # in the perimeter path
+    N_backpoints = np.size(x_perim)-x_0_index
+
+    #Create a variable to hold the parameterized path
+    x_par = np.zeros(np.size(x_perim)+1)
+    y_par = np.zeros(np.size(y_perim)+1)
+
+    #Populate the parameterized path variables
+    x_par[0] = x_par_start
+    x_par[1:N_backpoints+1] = x_perim[x_0_index:]
+    x_par[N_backpoints+1:] = x_perim[:x_0_index]
+    y_par[0] = y_par_start
+    y_par[1:N_backpoints+1] = y_perim[x_0_index:]
+    y_par[N_backpoints+1:] = y_perim[:x_0_index]
+
+    path = np.vstack((x_par, y_par))
+
+    if not closed:
+        path = np.hstack((path, path[:,[0]]))
+
     return path
 
 def pts_to_path(pts):
